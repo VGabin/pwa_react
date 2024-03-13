5eb575e6f64298ff76ea21743f337372f22d3928
VGabin <gabin.vallee@outlook.fr>
2024-03-13 15:43:41 +0100

    pwa update

--- front/public/sw.js
+++ front/public/sw.js
@@ -1,1 +1,52 @@
-console.warn("hey");
+const cacheData = "v1";
+
+this.addEventListener('install', (event) => {
+    this.skipWaiting();
+    event.waitUntil(
+        caches.open(cacheData).then((cache) => {
+            cache.addAll([
+                "/",
+                "/offline.html",
+                "/static/js/bundle.js",
+            ]);
+        }));
+    console.log(`${cacheData} Install`);
+});
+
+this.addEventListener('activate', (event) => {
+    clients.claim();
+    event.waitUntil((
+        async () => {
+            const keys = await caches.keys();
+            await Promise.all(
+                keys.map((key) => {
+                    if (!key.includes(cacheData)) {
+                        return caches.delete(key);
+                    };
+                })
+            );
+        })()
+    );
+    console.log(`${cacheData} Activate`);
+});
+
+this.addEventListener("fetch", (event) => {
+    console.log(`Fetching : ${event.request.url}, Mode : ${event.request.mode}`);
+    if (event.request.mode === "navigate") {
+        event.respondWith(
+            (async () => {
+                try {
+                    const preloadResponse = await event.preloadResponse;
+                    if (preloadResponse) {
+                        return preloadResponse;
+                    }
+
+                    return await fetch(event.request);
+                } catch (e) {
+                    const cache = await caches.open(cacheData);
+                    return await cache.match("/offline.html");
+                };
+            })()
+        );
+    }
+});
